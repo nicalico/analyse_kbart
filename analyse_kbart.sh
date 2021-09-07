b02323e6bffa24691bb755fd3d2ceb612714c22b
Nicolas Dickner <dickner.nicolas@uqam.ca>
2021-09-07 16:15:39 -0400

    Output plus compact

--- analyse_kbart.sh
+++ analyse_kbart.sh
@@ -36,25 +36,32 @@ for i in $(cat $fichier_coverage_depth_a_inclure | sed '/^[[:space:]]*$/d')
 cat $kbart | sed '/^[[:space:]]*$/d' > $fichier_output
 
 
-
-############ Collections à exclure
-
 lignes_kbart=$(wc -l < $fichier_output)
 
 echo -e "\n\n"
 echo $separateur
 echo -e "Items dans le fichier: $((lignes_kbart-1))"
 
+
+############ Statistiques
+
+# Mettre ceci où on veut
+echo $separateur
+echo "Répartitions du champ coverage_depth"
+awk -f stats.awk $fichier_output
+
+
+############ Collections à exclure
+
 echo $separateur
 echo -e "Filtrage des collections (case insensitive) :"
 
 for j in ${collections_a_exclure[@]}
 	do 
-		echo -e "  $j"
 		awk -v collection_a_exclure="$j" 'BEGIN {FS="\t"; OFS=FS; IGNORECASE=1} {if ($21 !~ collection_a_exclure) print $0}' $fichier_output > $buffer
 		n_output=$(wc -l < $fichier_output)
 		n_buffer=$(wc -l < $buffer)
-		echo -e "    items exclus: $((n_output-n_buffer))"
+		echo -e "    $j: $((n_output-n_buffer)) item exclus"
 		cat $buffer > $fichier_output
 	done
 
@@ -66,38 +73,30 @@ lignes_kbart_f=$(wc -l < $fichier_output)
 echo -e "\nItems après exclusions: $((lignes_kbart_f-1))"
 
 
-
 ############ Coverage depth à inclure
 
 echo $separateur
 echo "Filtrage du champ coverage_depth"
-n_buffer=0
-> $buffer
 
+> $buffer
 head -1 $fichier_output >> $buffer
+n_buffer=1
+
 
 for k in ${coverage_depth_a_inclure[@]}
 	do 
-		echo -e "  $k"
 		awk -v coverage_depth="$k" 'BEGIN {FS="\t"; OFS=FS; IGNORECASE=1} {if ($14 ~ coverage_depth) print $0}' $fichier_output >> $buffer
 		n_output=$(wc -l < $buffer)
-		echo -e "    items inclus: $((n_output-n_buffer))"
+		echo -e "    $k: $((n_output-n_buffer)) items inclus"
 		n_buffer=$(wc -l < $buffer)
 	done
+
+echo -e "\nItems après inclusions: $(($(wc -l < $buffer)-1))"
 	
 cat $buffer > $fichier_output
 
 
 cat $fichier_output > './output.avec.doublons.csv'
-
-
-############ Statistiques
-
-# Mettre ceci où on veut
-echo $separateur
-echo "Répartitions du champ coverage_depth"
-awk -f stats.awk $fichier_output
-
 
 
 ############ Dédoublonnage
