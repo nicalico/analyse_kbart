9b5d07426d7951a37a1ad9e163ac4779ba68b26f
Nicolas Dickner <dickner.nicolas@uqam.ca>
2021-09-07 15:24:41 -0400

    Refactorisation noms variables

--- analyse_kbart.sh
+++ analyse_kbart.sh
@@ -6,8 +6,9 @@ IFS=$'\n'
 touch "./buffer"
 
 kbart=$1
-liste_exclusions="./exclusions"
-output="./output.sans_doublons.csv"
+fichier_collections_a_exclure="./collections_a_exclure"
+fichier_coverage_depth_a_inclure="./coverage_depth_a_inclure"
+fichier_output="./output.sans.doublons.csv"
 buffer="./buffer"
 
 separateur=$(printf -- '-%.0s' {1..60})
@@ -16,22 +17,22 @@ separateur=$(printf -- '-%.0s' {1..60})
 
 ############ Listes des inclusions et exclusions
 
-declare -a exclusions
+declare -a collections_a_exclure
 
-dos2unix -q $liste_exclusions
+dos2unix -q $fichier_collections_a_exclure
 
-for i in $(cat $liste_exclusions | sed '/^[[:space:]]*$/d')
+for i in $(cat $fichier_collections_a_exclure | sed '/^[[:space:]]*$/d')
 	do 
-		exclusions+=("$i")
+		collections_a_exclure+=("$i")
 	done
 
-cat $kbart | sed '/^[[:space:]]*$/d' > $output
+cat $kbart | sed '/^[[:space:]]*$/d' > $fichier_output
 
 
 
 ############ Collections à exclure
 
-lignes_kbart=$(wc -l < $output)
+lignes_kbart=$(wc -l < $fichier_output)
 
 echo -e "\n\n"
 echo $separateur
@@ -40,21 +41,21 @@ echo -e "Items dans le fichier: $((lignes_kbart-1))"
 echo $separateur
 echo -e "Application des exclusions (case insensitive) :"
 
-for j in ${exclusions[@]}
+for j in ${collections_a_exclure[@]}
 	do 
 		echo -e "  $j"
-		awk -v exclusions="$j" 'BEGIN {FS="\t"; OFS=FS; IGNORECASE=1} {if ($21 !~ exclusions) print $0}' $output > $buffer
-		n_output=$(wc -l < $output)
+		awk -v collection_a_exclure="$j" 'BEGIN {FS="\t"; OFS=FS; IGNORECASE=1} {if ($21 !~ collection_a_exclure) print $0}' $fichier_output > $buffer
+		n_output=$(wc -l < $fichier_output)
 		n_buffer=$(wc -l < $buffer)
 		echo -e "    items exclus: $((n_output-n_buffer))"
-		cat $buffer > $output
+		cat $buffer > $fichier_output
 	done
 
 
 # Formattage possible
 #LC_NUMERIC=en_US printf "%'.f\n" $var
 
-lignes_kbart_f=$(wc -l < $output)
+lignes_kbart_f=$(wc -l < $fichier_output)
 echo -e "\nItems après exclusions: $((lignes_kbart_f-1))"
 
 
@@ -66,14 +67,14 @@ echo "Filtrage du champ coverage_depth"
 echo "  Filtres appliqués: fulltext, ebook et video"
 
 	# peut-on créer requête awk d'inclusion or en loopant sur un array plutôt que de hardcoder la requête?
-	awk -v requete="$req" 'BEGIN {FS="\t"; OFS=FS; IGNORECASE=1} {if ($14 ~ /coverage_depth/ || $14 ~ /fulltext/ || $14 ~ /ebook/ || $14 ~ /video/) print $0}' $output > $buffer
-	n_output=$(wc -l < $output)
+	awk -v requete="$req" 'BEGIN {FS="\t"; OFS=FS; IGNORECASE=1} {if ($14 ~ /coverage_depth/ || $14 ~ /fulltext/) print $0}' $fichier_output > $buffer
+	n_output=$(wc -l < $fichier_output)
 	n_buffer=$(wc -l < $buffer)
 	echo -e "    items exclus: $((n_output-n_buffer))"
-	cat $buffer > $output
+	cat $buffer > $fichier_output
 
 
-cat $output > './output.avec.doublons.csv'
+cat $fichier_output > './output.avec.doublons.csv'
 
 
 ############ Statistiques
@@ -81,7 +82,7 @@ cat $output > './output.avec.doublons.csv'
 # Mettre ceci où on veut
 echo $separateur
 echo "Répartitions du champ coverage_depth"
-awk -f stats.awk $output
+awk -f stats.awk $fichier_output
 
 
 
@@ -92,9 +93,9 @@ echo -e "Dédoublonnages"
 
 dedoublonnage()
 	{
-		awk -v champ=$1 'BEGIN {FS="\t"; OFS=FS; IGNORECASE=1} {if (length($champ) == 0 || !visited[$champ]++) print $0}' $output > $buffer
-		cat $buffer > $output
-		lignes_kbart_f=$(wc -l < $output)
+		awk -v champ=$1 'BEGIN {FS="\t"; OFS=FS; IGNORECASE=1} {if (length($champ) == 0 || !visited[$champ]++) print $0}' $fichier_output > $buffer
+		cat $buffer > $fichier_output
+		lignes_kbart_f=$(wc -l < $fichier_output)
 		echo -e "  Items après dédoublonnage: $((lignes_kbart_f-1))"
 	}
 
